47cc9dbc488b619ec324058c0cd12059d6447dc4
xuehui <xuehui@sunso>
2014-12-03 17:42:19 +0800

    update test

--- speed-test/src/test/java/me/power/speed/test/db/rxjavajdbc/TestQueryTable.java
+++ speed-test/src/test/java/me/power/speed/test/db/rxjavajdbc/TestQueryTable.java
@@ -8,6 +8,10 @@ import com.github.davidmoten.rx.jdbc.Database;
 
 import me.power.speed.test.AbstractSpringTest;
 
+//https://github.com/davidmoten/rxjava-jdbc
+//https://github.com/jcabi/jcabi-jdbc
+//https://github.com/jprante/elasticsearch-river-jdbc
+//https://github.com/johanbassonza/jdbc/tree/master/src/main/java/za/co/johanbasson/jdbc
 public class TestQueryTable extends AbstractSpringTest {
 	private String url = "jdbc:mysql://10.10.0.126:3307/gametenddata";
 	private String username = "gametenddata";
