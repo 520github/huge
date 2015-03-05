5538ad3795a822a9c0b5f4abfa58cdf46ba0d065
xuehui <xuehui.miao@tendcloud.com>
2015-03-05 10:48:34 +0800

    add synchronized list test

--- speed-test/src/main/java/me/power/speed/test/concurrent/sync/ListValueStringSynchronized.java
+++ speed-test/src/main/java/me/power/speed/test/concurrent/sync/ListValueStringSynchronized.java
@@ -9,6 +9,33 @@ public class ListValueStringSynchronized {
 	
 	private static int maxSize = 100;
 	
+	protected static void sleep(int sleepTime) {
+		try {
+			Thread.sleep(sleepTime);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void removeLastValue() {
+		synchronized (list) {
+			System.out.println("start remove last value");
+			sleep(20000);
+			int i = list.size()-1;
+			System.out.println("start remove last value:" + list.get(i));
+			list.remove(i);
+			System.out.println("end remove last value");
+		}
+	}
+	
+	public static void getLastValue() {
+		//synchronized (list) {
+			System.out.println("start get last value");
+			String value = list.get(list.size()-1);
+			System.out.println("get last value:" + value);
+		//}
+	}
+	
 	private static void addValue(String value) {
 		if(list.size() >= maxSize) {
 			System.out.println("go into threadName:" + Thread.currentThread().getName()+ ",value:" + value);
@@ -51,16 +78,36 @@ public class ListValueStringSynchronized {
 	protected static void testAddValueByThread() {
 		addValueByThread(1,100);
 		addValueByThread(200,302);
-	}
-	
-	public static void main(String[] args) {
-		testAddValueByThread();
-		//System.out.println(Thread.getAllStackTraces().size());
 		while(true) {
 			if(Thread.getAllStackTraces().size() ==5) {
 				printList(list);
 				break;
 			}
-		}		
+		}
+	}
+	
+	protected static void testRemoveAndGetLastValueByThread() {
+		addValueByThread(1,100);
+		sleep(20);
+		Thread td1 = new Thread(new Runnable() {
+			public void run() {
+				removeLastValue();
+			}
+		});
+		td1.start();
+		
+		sleep(20);
+		
+		Thread td2 = new Thread(new Runnable() {
+			public void run() {
+				getLastValue();
+			}
+		});
+		td2.start();
+	}
+	
+	public static void main(String[] args) {
+		//testAddValueByThread();		
+		testRemoveAndGetLastValueByThread();
 	}
 }
